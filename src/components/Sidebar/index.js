16819366a1b87fa4cfb14d01dbbf0fade0ca5e49
William <you@example.com>
2025-08-03 22:44:31 -0300

    fix: ajuste na sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,13 +1,14 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import avatarImg from "../../assets/avatar.png";
 import "./index.css";
 import { AuthContext } from "../../contexts/auth";
-import { FiHome, FiSettings, FiUser } from "react-icons/fi";
+import { FiHome, FiSettings, FiUser, FiPlus } from "react-icons/fi";
 import { AiOutlinePoweroff } from "react-icons/ai";
 
 export default function Sidebar() {
 	const { user, logout } = useContext(AuthContext);
+	const [open, setOpen] = useState(false);
 
 	async function handleLogOut() {
 		await logout();
@@ -23,18 +24,41 @@ export default function Sidebar() {
 					/>
 				</div>
 
-				<Link to="/receitas">
-					<FiHome color="#FFF" size={24} />
-					Receitas
-				</Link>
-				<Link to="/despesas">
+				{/* <div className="dropdown">
+					<button className="dropdown-toggle" onClick={() => setOpen(!open)}>
+						<FiPlus color="#FFF" size={24} />
+						Cadastros
+					</button>
+
+					{open && (
+						<div className="dropdown-menu">
+							<Link to="/newReceita">Nova Receita</Link>
+							<Link to="/newDespesa">Nova Despesa</Link>
+							<Link to="/categorias">Nova Categoria</Link>
+						</div>
+					)}
+				</div> */}
+				<nav className="dropdown">
+					<button className="dropdown-toggle" onClick={() => setOpen(!open)}>
+						<FiPlus color="#FFF" size={24} />
+						Cadastros
+					</button>
+					{open && (
+						<p className="dropdown-menu">
+							<Link to="/receitas">Receitas</Link>
+							<Link to="/newDespesa">Despesas</Link>
+							<Link to="/categorias">Categorias</Link>
+						</p>
+					)}
+				</nav>
+				{/* <Link to="/despesas">
 					<FiHome color="#FFF" size={24} />
 					Despesas
 				</Link>
 				<Link to="/categorias">
 					<FiUser color="#FFF" size={24} />
 					Categoria
-				</Link>
+				</Link> */}
 				<Link to="/profile">
 					<FiSettings color="#FFF" size={24} />
 					Perfil
