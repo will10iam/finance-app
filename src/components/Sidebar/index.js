e51a9f4708b5445ba10a8006f6747b7be8a12ebc
William <you@example.com>
2026-03-18 22:01:04 -0300

    style: mudando logo e aplicando no sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,55 +1,8 @@
-/* import { useState } from "react";
-import "./index.css";
-import LogOut from "../LogOutButton";
-
-export default function Sidebar({ isOpen, onClose }) {
-	const [openCadastro, setOpenCadastro] = useState(false);
-
-	return (
-		<>
-			{/* Overlay 
-			{isOpen && <div className="sidebar-overlay" onClick={onClose} />}
-
-			<aside className={`sidebar ${isOpen ? "open" : ""}`}>
-				<div className="sidebar-header">
-					<h1 className="logo">MeuApp</h1>
-				</div>
-
-				<nav className="sidebar-nav">
-					<a href="/dashboard">Dashboard</a>
-					<a href="/transacoes">Transações</a>
-
-					<button
-						className="dropdown-toggle"
-						onClick={() => setOpenCadastro(!openCadastro)}
-					>
-						Cadastros
-						<span className={`arrow ${openCadastro ? "open" : ""}`}>▾</span>
-					</button>
-
-					{openCadastro && (
-						<div className="dropdown-menu">
-							<a href="/categorias">Categoria</a>
-							<a href="/newReceita">Nova Receita</a>
-							<a href="/newDespesa">Nova Despesa</a>
-							<a href="/saldos">Saldos</a>
-						</div>
-					)}
-				</nav>
-
-				<div className="sidebar-footer">
-					<LogOut />
-				</div>
-			</aside>
-		</>
-	);
-}
- */
-
 import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./index.css";
 import LogOut from "../LogOutButton";
+import logo from "../../assets/logo5.png";
 
 export default function Sidebar({ isOpen, onClose }) {
 	const [openCadastro, setOpenCadastro] = useState(false);
@@ -86,8 +39,9 @@ export default function Sidebar({ isOpen, onClose }) {
 			>
 				<div className="sidebar-header">
 					<div className="sidebar-brand">
-						<h1 className="sidebar-logo">MeuApp</h1>
-						<span className="sidebar-subtitle">Finance</span>
+						{/* <h1 className="sidebar-logo">MeuApp</h1>
+						<span className="sidebar-subtitle">Finance</span> */}
+						<img src={logo} alt="" className="sidebar-logo" />
 					</div>
 
 					<button
