9703f302266ff6ab9bc889aa09136cd9dbd703f2
William <you@example.com>
2026-04-06 20:56:12 -0300

    feat: add suporte PWA (service worker, manifest, installable app)

---
+++ public/service-worker.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-restricted-globals */
+// eslint-disable-next-line no-unused-vars
+const CACHE_NAME = "seventwofinn-v1";
+
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener("install", (event) => {
+	console.log("Service Worker instalado");
+});
+
+self.addEventListener("activate", (event) => {
+	console.log("Service Worker ativado");
+});
+
+self.addEventListener("fetch", (event) => {
+	event.respondWith(fetch(event.request));
+});
